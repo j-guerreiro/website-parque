38734f6fb6522726973dbc12752254e28a8dbae0
j-guerreiro <joel.borghi@gmail.com>
2022-02-12 17:30:06 -0300

    update - a implementar menu mobile

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,8 +1,1 @@
-function abreMenu() {
-  let navMenu = document.getElementById("nav-inner");
-  if ( navMenu.x.style.display === "block"){
-    navMenu.style.display = "none";
-  } else {
-    navMenu.style.display = "block";
-  }
-}
+//a implementar
